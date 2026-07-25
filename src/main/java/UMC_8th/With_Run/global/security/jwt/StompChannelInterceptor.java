d3bf3651f2ad69f894c81931142519f92fe818fd
jijysun <jijysun@gmail.com>
2026-07-26 02:25:50 +0900

    DOCKER - 컨테이너 타임존 통일 (KST)

--- src/main/java/UMC_8th/With_Run/global/security/jwt/StompChannelInterceptor.java
+++ src/main/java/UMC_8th/With_Run/global/security/jwt/StompChannelInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
